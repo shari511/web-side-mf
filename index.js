142984627e96b2c4fda44f7964a16d1e3952cd02
shari511 <81506058+shari511@users.noreply.github.com>
2023-10-07 12:46:13 -0300

    web side 2
    222222222222222222

---
+++ index.js
@@ -0,0 +1,18 @@
+let nombre = prompt("escriba su nombre");
+console.log("hola " + nombre);
+let edad = parseInt(prompt("escriba su edad"));
+console.log(edad);
+
+if(edad>=18){
+    console.log(edad);
+}else{
+    console.log("NO")}
+
+ {
+        height: 500px;
+        background-image: url("https://carontestudio.com/img/caronte-web-estudio-logo.png");
+        background-repeat:no-repeat;
+        background-position-x:center;
+        background-position-y:center;
+        background-size: 10rem;
+      }
